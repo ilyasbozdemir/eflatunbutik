398d2f534f40c75d873c3440bb3b5407a2d1fd0c
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-11-22 17:00:38 +0300

    updated

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+var cors = require('cors')
+
+ 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is up on port ${port}`)
 })
