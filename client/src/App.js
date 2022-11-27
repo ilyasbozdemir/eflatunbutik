63d73fc9ce6ac337b2daeb424efa8d300ab0517d
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-11-27 17:01:48 +0300

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,22 @@ import Layout from "./Layout";
 import Footer from "./components/Footer";
 import Router from "./Router";
 import { Provider } from "react-redux";
-import  store  from "./Store";
+import store from "./Store";
+import Toast from "./components/Toast";
+import Modal from "./components/Modal";
+
+
 function App() {
+
+
+
   return (
     <>
       <Provider store={store}>
         <Layout>
+          <>
+          
+          </>
           <Router />
           <Footer />
         </Layout>
