34c55227772d076b2f8a65aea20ca4f0c370e3ca
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-03 18:11:58 +0300

    updated

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,23 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
-
-
-import Favorite from "./pages/Favorite";
-import Basket from "./pages/Basket";
-import Login from "./pages/Login";
-
-import Dashboard from "./pages/Dashboard";
-import Catalog from "./pages/Dashboard/Catalog";
-import Order from "./pages/Dashboard/Order";
-import User from "./pages/Dashboard/User";
-import UserLogin from "./pages/Dashboard/UserLogin";
-
-import RequireAuth from "../src/components/RequireAuth";
-
-import CategoryProduct from "./pages/CategoryProduct";
-
 const Home = React.lazy(() => import("./pages/Home"));
 const Page404 = React.lazy(() => import("./pages/ErrorPage/Page404"));
+const Favorite = React.lazy(() => import("./pages/Favorite"));
+const Basket = React.lazy(() => import("./pages/Basket"));
+const Login = React.lazy(() => import("./pages/Login"));
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Catalog = React.lazy(() => import("./pages/Dashboard/Catalog"));
+const Order = React.lazy(() => import("./pages/Dashboard/Order"));
+
+const User = React.lazy(() => import("./pages/Dashboard/User"));
+const UserLogin = React.lazy(() => import("./pages/Dashboard/UserLogin"));
+const RequireAuth = React.lazy(() => import("../src/components/RequireAuth"));
+const CategoryProduct = React.lazy(() => import("./pages/CategoryProduct"));
+
 
 function Router() {
   return (
