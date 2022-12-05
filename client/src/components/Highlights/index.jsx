acbf3263fa9e43d5dfddd0e79e825329aa53b55b
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-05 21:13:44 +0300

    updated

--- client/src/components/Highlights/index.jsx
+++ client/src/components/Highlights/index.jsx
@@ -34,7 +34,7 @@ function index() {
     const { name, onClickHandled } = props;
     return (
       <Button
-        fontSize={{ base: "12px", md: "20px" }}
+        size={{ base: "sm", md: "md" }}
         color={"white"}
         bgGradient={"linear(to-l, #7928CA, #FF0080)"}
         _hover={{
