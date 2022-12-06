0415dac81509213332d2b45d6d7526865d39ea9a
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-06 13:36:52 +0300

    updaterfd

--- client/src/components/Header/SearchBox.jsx
+++ client/src/components/Header/SearchBox.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Input, useBreakpointValue } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
 import { encode, decode } from "html-entities";
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -5,10 +5,12 @@ import {
   Stack,
   Text,
   Box,
+  Flex,
   Spacer,
   Divider,
   useDisclosure as UseDisclosure,
   useColorModeValue as UseColorModeValue,
+  useBreakpointValue as UseBreakpointValue,
 } from "@chakra-ui/react";
 
 import { Link } from "react-router-dom";
@@ -17,6 +19,7 @@ import Logo from "../Logo";
 import HeaderTop from "./HeaderTop";
 import HeaderBrand from "./HeaderBrand";
 import HeaderMenu from "./HeaderMenu";
+import SearchBox from "./SearchBox";
 
 import SettingSidebar from "../../components/SettingSidebar";
 import SettingSidebarButton from "../../components/SettingSidebar/SettingSidebarButton";
@@ -39,47 +42,57 @@ function index({ onOpen, ...rest }) {
         top="0"
         zIndex="1000"
         alignItems="center"
-        justifyContent={{ base: "flex-start", md: "flex-end" }}
         bg={UseColorModeValue("white", "blackAlpha.50")}
-        backdropFilter="blur(1.5rem)"
         boxShadow={
           "rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px"
         }
         {...rest}
       >
-        <IconButton
-          display={{ base: "flex", md: "none" }}
-          onClick={onOpen}
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiMenu />}
-          bg="transparent"
-        />
-
-        <Text
-          as="div"
-          display={{ base: "flex", md: "none" }}
-          fontSize="2xl"
-          fontFamily="monospace"
-          fontWeight="bold"
+        <Flex
+          direction={"column"}
+          alignItems="center"
+          justifyContent={{ base: "space-between", md: "flex-end" }}
+        
         >
-          <Link to="/">
-            <Logo />
-          </Link>
-        </Text>
-        <Spacer />
+          <Box display={"flex"} bg={"yellow.400"}>
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              onClick={onOpen}
+              variant="ghost"
+              aria-label="open menu"
+              icon={<FiMenu />}
+              bg="transparent"
+            />
 
-        {/*settings component*/}
-        <Box display={{ base: "block", md: "block", lg: "none" }}>
-          <SettingSidebarButton onOpen={onOpenSettingSidebar} />
-        </Box>
+            <Text
+              as="div"
+              display={{ base: "flex", md: "none" }}
+              fontSize="2xl"
+              fontFamily="monospace"
+              fontWeight="bold"
+            >
+              <Link to="/">
+                <Logo />
+              </Link>
+            </Text>
+            <Spacer />
 
-        <SettingSidebar
-          onOpen={onOpenSettingSidebar}
-          isOpen={isOpenSettingSidebar}
-          onClose={onCloseSettingSidebar}
-        />
-        {/**/}
+            {/*settings component*/}
+            <Box display={{ base: "block", md: "block", lg: "none" }}>
+              <SettingSidebarButton onOpen={onOpenSettingSidebar} />
+            </Box>
+            <Spacer />
+            <SettingSidebar
+              onOpen={onOpenSettingSidebar}
+              isOpen={isOpenSettingSidebar}
+              onClose={onCloseSettingSidebar}
+            />
+          </Box>
+
+          <Box display={{ base: "block", md: "none" }}>
+            <SearchBox />
+          </Box>
+        </Flex>
 
         <Stack display={{ base: "none", md: "flex" }} w={"100%"}>
           <HeaderTop />
@@ -88,6 +101,7 @@ function index({ onOpen, ...rest }) {
           <HeaderMenu />
         </Stack>
       </Box>
+
       {/*= Mobile Bottom Navigation =*/}
       <Box
         display={{ base: "block", md: "none" }}
