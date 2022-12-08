acc89205ebef976488113cf7e515114bbc10add8
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-08 21:53:17 +0300

    updated

--- client/src/components/Showcase/Showcase2/index.jsx
+++ client/src/components/Showcase/Showcase2/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Image, Flex } from "@chakra-ui/react";
+import React from "react";
+
+//https://images.unsplash.com/photo-1630759072462-d5348e577ee8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80
+//https://images.unsplash.com/photo-1548306530-3ece53b754b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80
+//https://images.unsplash.com/photo-1613521973937-efce73f2f943?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=930&q=80
 
 function index() {
+  const url =
+    "https://images.unsplash.com/photo-1630759072462-d5348e577ee8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80";
   return (
     <>
-     asd
+      <Flex >
+        <Image
+          ml={3}
+          mr={3}
+          src={'url'}
+          w={{ base: "437px" }}
+          wh={{ base: "640px" }}
+          rounded={"16px"}
+        ></Image>
+      </Flex>
     </>
-  )
+  );
 }
 
-export default index
+export default index;
