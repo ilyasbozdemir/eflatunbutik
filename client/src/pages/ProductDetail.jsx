11bfa55872a6cc293b6da9a3b9aa4cb889497629
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-16 12:18:14 +0300

    uu

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function ProductDetail() {
   const params = useParams();
-  alert(params.productDetail, params.productId);
+  //alert(params.productDetail, params.productId);
 
   return <div>ProductDetail</div>;
 }
