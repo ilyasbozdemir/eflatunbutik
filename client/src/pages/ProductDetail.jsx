3fc63fee931ef274aaf13a91116d83d323568d0d
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2022-12-18 17:00:31 +0300

    000

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,11 +1,11 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Text, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 function ProductDetail() {
   const params = useParams();
   const [isProduct, setIsProduct] = React.useState(true);
-
+  const [isDesktop] = useMediaQuery("(min-width: 768px)");
   //alert(params.productDetail, params.productId);
 
   if (isProduct)
@@ -13,29 +13,21 @@ function ProductDetail() {
 
     return (
       <>
-        <Button
-          onClick={async () => {
-            try {
-              const shareData = {
-                title: 'test title',
-                text: 'test text ',
-                url: 'http://localhost:3000/p/01-bamboo-tan'
-              }
-              await navigator.share(shareData);
-            } catch (err) {
-
-            }
-          }}
-        >
-          Paylaş
-        </Button>
-
-        <Box id="product-container">
-          <Box id="container-left-content">
-            <Box id="gallery-container"></Box>
+        {isDesktop === true ? (
+          <Box id="product-container">
+            <Box id="container-left-content">
+              <Box id="gallery-container">desktop</Box>
+            </Box>
+            <Box id="container-right-content"></Box>
           </Box>
-          <Box id="container-right-content"></Box>
-        </Box>
+        ) : (
+          <Box id="product-container">
+            <Box id="container-left-content">
+              <Box id="gallery-container">mobile</Box>
+            </Box>
+            <Box id="container-right-content"></Box>
+          </Box>
+        )}
       </>
     );
   else return <div>NotFound</div>;
