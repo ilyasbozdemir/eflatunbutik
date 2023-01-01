cd9ac4d2f57cdf8f1db9acbb9a71a7f090f59836
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-01 20:45:12 +0300

    updated

---
+++ client/src/components/PaymentCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Heading,
+  Stack,
+  Divider,
+  Button,
+  Image,
+  Center,
+  Flex,
+} from "@chakra-ui/react";
+
+import { BiEdit } from "react-icons/bi";
+import { IconButton } from "@chakra-ui/react";
+function PaymentCard(props) {
+  const { title, addressContent, editButtonClick } = props;
+  return (
+    <>
+      <Card
+        w={{ base: "100%", md: 150 }}
+        maxW={{ base: "100%", md: 200 }}
+        maxH={{ base: "100%", md: 300 }}
+        m={1}
+      >
+        <CardHeader fontWeight={"semibold"}>
+          <Flex justifyContent={"space-between"}>
+            <Text> {title}</Text>
+            <IconButton
+              colorScheme="pink"
+              aria-label="edit address"
+              icon={<BiEdit />}
+              variant={"ghost"}
+              onClick={editButtonClick}
+            />
+          </Flex>
+        </CardHeader>
+        <Divider />
+        <CardBody
+          overflowY={"scroll"}
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "4px",
+            },
+            "&::-webkit-scrollbar-track": {
+              width: "5px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "#444",
+              borderRadius: "12px",
+            },
+          }}
+        >
+          {addressContent}
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default PaymentCard;
--- client/src/pages/ProductPayment.jsx
+++ client/src/pages/ProductPayment.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Flex,
+  Stack,
+  Text,
+  useColorModeValue,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import React from "react";
+import CartOrderSummaryPayment from "../components/CartOrderSummaryPayment";
+import { useNavigate } from "react-router";
+import { FaArrowRight } from "react-icons/fa";
+
+import PaymentCard from "../components/PaymentCard";
 
 function ProductPayment() {
+  const navigate = useNavigate();
   return (
-    <div>ProductPayment</div>
-  )
+    <>
+      <Box maxW={{ base: "3xl", lg: "7xl" }}>
+        <Stack
+          direction={{
+            base: "column",
+            lg: "row",
+          }}
+          align={{
+            lg: "flex-start",
+          }}
+          spacing={{
+            base: "8",
+            md: "16",
+          }}
+        >
+          <Stack
+            spacing={{
+              base: "8",
+              md: "10",
+            }}
+            flex="2"
+          >
+            <Stack spacing="6">
+              <Wrap>
+                <WrapItem>
+                  <PaymentCard
+                    title={"title"}
+                    addressContent={"addressContent"}
+                    editButtonClick={"editButtonClick"}
+                  />
+                </WrapItem>
+                <WrapItem>
+                  <PaymentCard
+                    title={"title"}
+                    addressContent={"addressContent"}
+                    editButtonClick={"editButtonClick"}
+                  />
+                </WrapItem>
+                <WrapItem>
+                  <PaymentCard
+                    title={"title"}
+                    addressContent={"addressContent"}
+                    editButtonClick={"editButtonClick"}
+                  />
+                </WrapItem>
+              </Wrap>
+            </Stack>
+          </Stack>
+
+          <Flex direction="column" flex="1">
+            <CartOrderSummaryPayment />
+            <Button
+              width="95%"
+              variant="primary"
+              color={"white"}
+              bg={useColorModeValue("gray.100", "gray.700")}
+              bgGradient={"linear(to-l, #7928CA, #FF0080)"}
+              _hover={{
+                opacity: 0.9,
+              }}
+              cursor={"pointer"}
+              onClick={() => {}}
+            >
+              Siparişi Tamamla
+            </Button>
+          </Flex>
+        </Stack>
+      </Box>
+    </>
+  );
 }
 
-export default ProductPayment
+export default ProductPayment;
