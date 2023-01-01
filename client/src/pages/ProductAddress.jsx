e10b5bfec1ac3bdbc9522c9a0d8124e47b8c95ff
İlyas Bozdemir <Bozdemir.ib70@gmail.com>
2023-01-01 18:20:21 +0300

    updated

--- client/src/pages/ProductAddress.jsx
+++ client/src/pages/ProductAddress.jsx
@@ -5,16 +5,29 @@ import {
   IconButton,
   Input,
   Stack,
+  useColorModeValue,
+  useDisclosure,
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
 import AddressCard from "../components/AddressCard";
 import CartOrderSummaryAddress from "../components/CartOrderSummaryAddress";
 
 function ProductAddress() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Box maxW={{ base: "3xl", lg: "7xl" }} py={3}>
@@ -43,16 +56,20 @@ function ProductAddress() {
                 direction={{
                   sm: "row",
                 }}
-                justifyContent={'center'}
+                justifyContent={"center"}
               >
                 <Button
                   variant={"ghost"}
                   color={"white"}
                   bg={"pink.500"}
                   _hover={{ color: "white", bg: "pink.700" }}
+                  onClick={onOpen}
                 >
                   Yeni Adres Ekle
                 </Button>
+
+                <SetModal isOpen={isOpen} onClose={onClose} />
+
                 <Stack
                   direction={{
                     sm: "row",
@@ -61,7 +78,11 @@ function ProductAddress() {
                     lg: "360px",
                   }}
                 >
-                  <Input placeholder="Ara" type="text" w={"full"} />
+                  <Input
+                    placeholder="Ara"
+                    type="text"
+                    w={{ base: "full", md: "350px" }}
+                  />
                   <IconButton
                     color="pink.500"
                     type="submit"
@@ -93,6 +114,19 @@ function ProductAddress() {
 
           <Flex direction="column" flex="1">
             <CartOrderSummaryAddress />
+            <Button
+              width="95%"
+              variant="primary"
+              color={"white"}
+              bg={useColorModeValue("gray.100", "gray.700")}
+              bgGradient={"linear(to-l, #7928CA, #FF0080)"}
+              _hover={{
+                opacity: 0.9,
+              }}
+              cursor={"pointer"}
+            >
+              Devam Et
+            </Button>
           </Flex>
         </Stack>
       </Box>
@@ -100,4 +134,25 @@ function ProductAddress() {
   );
 }
 
+function SetModal({ isOpen, onClose }) {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>jkllkjlj</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 export default ProductAddress;
